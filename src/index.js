232a42c590bd099fd7d9d1cd988a9a37c97bd1fa
DJ Pixel <lilp1xel1@gmail.com>
2022-03-21 15:04:59 +1100

    Removed regular commands, added / commands

--- src/index.js
+++ src/index.js
@@ -1,38 +1,22 @@
-const { Client, Intents, Collection, MessageEmbed, Guild, Message} = require("discord.js")
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS] });
-const dotenv = require(`dotenv`)
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
+dotenv = require("dotenv")
 dotenv.config()
-const fs = require('fs')
-const { GiveawaysManager } = require('discord-giveaways');
-client.on("ready", () => { //when bot starts
-console.log("I'm online!") //console info
-const botver = "1.0.0"; //bot version string
-const serverNumb = client.guilds.cache.size //records how many servers the bot is in
-const botProfile = client.user.tag //records the username and discriminator the bot is logged in as
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on('debug', console.log) //log debug messages
-      .on('warn', console.log) //log warning messages
-console.log('(Version ' + botver + ')');	//console info
-console.log('Logged in as ' + client.user.tag)//console info
-client.aliases = new Collection() //command handler
-client.commands = new Collection() //command handler
-require('./commandhandler')(client) //command handler
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
 
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const dateObj = new Date();
-const month = monthNames[dateObj.getMonth()];
-const day = String(dateObj.getDate()).padStart(2, '0');
-const year = dateObj.getFullYear();
-const botstarted = month +" " + day  + ', ' + year;
-const hours = dateObj.getHours()
-const minutes = dateObj.getMinutes()
-const seconds = dateObj.getSeconds()
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-console.log('Bot started at ' + botstarted + " at " + hours + ":" + minutes + ":" + seconds)
 
 /* 
 Giveaway Start
@@ -59,66 +43,23 @@ client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
     console.log(`Giveaway #${giveaway.messageID} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
 });
 
-/*
-Giveaway End
-*/
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
-const info = {
-    dateStarted: botstarted,
-    timeStarted: hours + ":" + minutes + ":" + seconds,
-    botver: botver,
-    serverNumb: serverNumb,
-    botProfile: botProfile 
-}
+	const command = client.commands.get(interaction.commandName);
 
-const jsonString = JSON.stringify(info, null, 2)
-console.log(jsonString)
+	if (!command) return;
 
-fs.writeFile('info.json', jsonString, err => {
-    if (err) {
-        console.log('Error writing file', err)
-    } else {
-        console.log('Successfully wrote file')
-    }
-})});
-
-client.on('ready', () => { //when bot starts
-    client.user.setStatus('available') //change bot online/dnd/idle/offlien to online
-    client.user.setActivity("https://github.com/essentials-bot/essentials", {
-        type: "PLAYING"
-      });
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
-client.on("messageCreate", Message => {  //when a message is sent
 
-    let Prefix = "+"
-    let Args = Message.content.slice(Prefix.length).trim().split(/ +/g); //set args
-    let Command = Args.shift().toLowerCase(); //turns Args into Command
-    
-    if (Message.content.startsWith(Prefix)) { //if a command is sent
-        let CommandFile = client.commands.get(Command) || client.commands.get(client.aliases.get(Command)); //get the commandfile of the command
+const release = 0;
 
-        if (CommandFile) { //if the commandfile exists
-            CommandFile.run(client, Message, Args); //run the commandfile
-        }     
-    } else {} //otherwise, do nothing
-});
-
-/*
-^ Commands
-
-v Bot startup
-*/
-
-
-
-const releaseVer = 0 //Stable-0 Beta-1
-
-if (releaseVer == 1) {
-	client.login(process.env.TOKEN2); //beta
-	console.log('Logging in under Beta token...') //log beta token login
-}
-else if (releaseVer == 0) {
-	client.login(process.env.TOKEN); //stable
-	console.log('Logging in under Stable token...') //log stable token login
-}
+if (release === 0) console.log("LOGGING INTO STABLE CLIENT"), Client.login(process.env.TOKEN);
+if (release === 1) console.log("LOGGING INTO BETA CLIENT"), Client.login(process.env.TOKEN2);
