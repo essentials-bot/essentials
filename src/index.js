9a5d5102266654d43bdb1ff2e42d32c6c083680f
DJ Pixel <lilp1xel1@gmail.com>
2022-03-23 11:54:17 +1100

    Beautified index.js

--- src/index.js
+++ src/index.js
@@ -11,22 +11,30 @@ const {
     GiveawaysManager
 } = require('discord-giveaways');
 
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS]
+});
+
+
+const {
+    REST
+} = require('@discordjs/rest');
+const {
+    Routes
+} = require('discord-api-types/v9');
 
 
 const commands = [];
 
-// Place your client and guild ids here
 const clientId = '955262542471643196';
 const guildId = '906468430494973954';
 
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+const rest = new REST({
+    version: '9'
+}).setToken(process.env.TOKEN);
 
 
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS]
-});
+
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -36,13 +44,52 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
+
 // Constants Done
+
+
 
 client.once('ready', () => {
     console.log('Ready!');
     client.user.setActivity("https://discord.gg/cQk2msf9pQ", {
         type: "PLAYING",
     })
+
+    const botver = "1.0.0"; //bot version string
+    const serverNumb = client.guilds.cache.size //records how many servers the bot is in
+    const botProfile = client.user.tag //records the username and discriminator the bot is logged in as
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const dateObj = new Date();
+    const month = monthNames[dateObj.getMonth()];
+    const day = String(dateObj.getDate()).padStart(2, '0');
+    const year = dateObj.getFullYear();
+    const botstarted = month + " " + day + ', ' + year;
+    const hours = dateObj.getHours()
+    const minutes = dateObj.getMinutes()
+    const seconds = dateObj.getSeconds()
+    const djp = "364902391717298181"
+    const amplex = "245773250154594304"
+
+    const info = {
+        dateStarted: botstarted,
+        timeStarted: hours + ":" + minutes + ":" + seconds,
+        botver: botver,
+        serverNumb: serverNumb,
+        botProfile: botProfile,
+        djp: `<@${djp}>`,
+        amplex: `<@${amplex}>`,
+    }
+
+    const jsonString = JSON.stringify(info, null, 2)
+
+    fs.writeFile('info.json', jsonString, err => {
+        if (err) {
+            console.log('Error writing file', err)
+        } else {
+            console.log('Successfully wrote file')
+        }
+    })
+
 });
 
 /*
@@ -50,23 +97,24 @@ Refresh Start
 */
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 (async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
+    try {
+        console.log('Started refreshing application (/) commands.');
 
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId), {
+                body: commands
+            },
+        );
 
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
 })();
 
 
